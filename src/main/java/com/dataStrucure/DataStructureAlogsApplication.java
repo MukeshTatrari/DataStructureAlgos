2d29f8316303dc0c40af105c49667712b9901bd6
Chandra <mukesh.chandra@capgemini.com>
2022-07-05 20:09:17 +0530

    initial commit

---
+++ src/main/java/com/dataStrucure/DataStructureAlogsApplication.java
@@ -0,0 +1,13 @@
+package com.dataStrucure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAlogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataStrucure/DataStructureAlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataStrucure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureAlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
