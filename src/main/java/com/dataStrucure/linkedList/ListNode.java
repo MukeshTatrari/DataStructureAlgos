2163cdc19194d8116920f3828376bbd79f1bc75b
Chandra <mukesh.chandra@capgemini.com>
2022-07-08 11:03:07 +0530

    More changes in  linked list

--- src/main/java/com/dataStrucure/linkedList/ListNode.java
+++ src/main/java/com/dataStrucure/linkedList/ListNode.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -114,5 +116,225 @@ public class ListNode {
             previous.next = previous.next.next;
             return head;
         }
+    }
+
+    public boolean searchKey(ListNode head, int searchKey) {
+        if (head == null) {
+            return false;
+        } else {
+            while (head != null) {
+                if (searchKey == head.data) {
+                    return true;
+                }
+                head = head.next;
+            }
+        }
+        return false;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null) {
+            return head;
+        } else {
+            ListNode currentNode = head;
+            ListNode previousNode = null;
+            ListNode nextNode = null;
+            while (currentNode != null) {
+                nextNode = currentNode.next;
+                currentNode.next = previousNode;
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+            return previousNode;
+            // 1-->4-->11-->18-->null
+        }
+    }
+
+    public ListNode findMiddleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fastPoint = head;
+        ListNode slowPointer = head;
+        while (fastPoint != null && fastPoint.next != null) {
+            slowPointer = slowPointer.next;
+            fastPoint = fastPoint.next.next;
+        }
+        return slowPointer;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode current = head;
+        int count = 1;
+        while (current != null) {
+            current = current.next;
+            count++;
+        }
+        int middle = count / 2;
+        int position = 1;
+        ListNode middleNode = head;
+        if (middle != 1) {
+            while (position <= middle) {
+                middleNode = middleNode.next;
+                position++;
+            }
+        }
+        return middleNode;
+    }
+
+    public ListNode findNthNodeFromEnd(ListNode head, int position) {
+        // 1-->4-->11-->18-->null
+        if (head == null) {
+            return null;
+        } else {
+            ListNode mainPtr = head;
+            ListNode refPtr = head;
+            int count = 0;
+            while (count < position) {
+                refPtr = refPtr.next;
+                count++;
+            }
+            while (refPtr != null) {
+                refPtr = refPtr.next;
+                mainPtr = mainPtr.next;
+            }
+            return mainPtr;
+        }
+    }
+
+    public ListNode findNthNodeFromEnd2(ListNode head, int position) {
+        if (head == null) {
+            return null;
+        }
+        ListNode reverse = reverse(head);
+        int count = 1;
+        while (count < position) {
+            reverse = reverse.next;
+            count++;
+        }
+        return reverse;
+    }
+
+    public void removeDuplicateFromSortedList(ListNode head) {
+        if (head == null) {
+            return;
+        } else {
+            ListNode current = head;
+            while (current != null && current.next != null) {
+                if (current.data == current.next.data) {
+                    current.next = current.next.next;
+                } else {
+                    current = current.next;
+                }
+            }
+        }
+    }
+
+    public ListNode insertInSortedLinkedList(ListNode head, int data) {
+        ListNode node = new ListNode(data);
+        if (head == null) {
+            head = node;
+            return head;
+        } else {
+            ListNode current = head;
+            ListNode temp = null;
+            while (current != null && current.data < data) {
+                temp = current;
+                current = current.next;
+            }
+            node.next = current;
+            temp.next = node;
+        }
+
+        return head;
+    }
+
+    public void removeGivenNode(ListNode head, int key) {
+        if (head == null) {
+            return;
+        } else {
+            ListNode current = head;
+            ListNode temp = null;
+            while (current != null && current.data != key) {
+                temp = current;
+                current = current.next;
+            }
+            if (current == null) {
+                return;
+            } else {
+                temp.next = current.next;
+            }
+        }
+    }
+
+    public boolean checkIfLoop(ListNode head) {
+        if (head == null) {
+            return false;
+        } else {
+            ListNode fastPtr = head;
+            ListNode slowPtr = head;
+            while (fastPtr != null && fastPtr.next != null) {
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next.next;
+                if (slowPtr == fastPtr) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public ListNode startingNodeOfLoop(ListNode head) {
+        if (head == null) {
+            return null;
+        } else {
+            ListNode fastPtr = head;
+            ListNode slowPtr = head;
+            while (fastPtr != null && fastPtr.next != null) {
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next.next;
+                if (slowPtr == fastPtr) {
+                    return getStartingNode(head, slowPtr);
+                }
+            }
+        }
+        return null;
+    }
+
+    public ListNode getStartingNode(ListNode head, ListNode slowPtr) {
+        ListNode temp = head;
+        while (slowPtr != temp) {
+            temp = temp.next;
+            slowPtr = slowPtr.next;
+        }
+        return temp;
+    }
+
+    public void removeLoopIfPresent(ListNode head) {
+        if (head == null) {
+            return;
+        } else {
+            ListNode fastPtr = head;
+            ListNode slowPtr = head;
+            while (fastPtr != null && fastPtr.next != null) {
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next.next;
+                if (slowPtr == fastPtr) {
+                    removeLoop(head, slowPtr);
+                }
+            }
+        }
+    }
+
+    public void removeLoop(ListNode head, ListNode slowPtr) {
+        ListNode temp = head;
+        while (temp.next != slowPtr.next) {
+            head = head.next;
+            slowPtr = slowPtr.next;
+        }
+        slowPtr.next = null;
     }
 }
--- src/main/java/com/dataStrucure/linkedList/SinglyLinkedList.java
+++ src/main/java/com/dataStrucure/linkedList/SinglyLinkedList.java
@@ -1,41 +1,60 @@
 package com.dataStrucure.linkedList;
 
-import java.util.List;
-
 public class SinglyLinkedList {
 
     private ListNode head;
 
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-        singlyLinkedList.head = new ListNode(10);
-        ListNode secondListNode = new ListNode(1);
+        singlyLinkedList.head = new ListNode(2);
+        ListNode secondListNode = new ListNode(5);
         ListNode thirdListNode = new ListNode(8);
         ListNode forthListNode = new ListNode(11);
 
         singlyLinkedList.head.next = secondListNode;
         secondListNode.next = thirdListNode;
         thirdListNode.next = forthListNode;
+        forthListNode.next = secondListNode;
 
         ListNode ls = new ListNode();
-        ls.print(singlyLinkedList.head);
-        int length = ls.countElement(singlyLinkedList.head);
-        System.out.println("total elements in list ::: " + length);
-        ListNode newHead = ls.insertAtBeginning(singlyLinkedList.head, 20);
-        ls.print(newHead);
-        newHead = ls.insertAtBeginning(newHead,30);
-        ls.print(newHead);
-        ls.insertAtEnd(singlyLinkedList.head, 60);
-        ls.print(singlyLinkedList.head);
-        ls.insertAtGivenPosition(singlyLinkedList.head, 2,96);
-        ls.print(singlyLinkedList.head);
-        newHead = ls.deleteNodeFromBeginning(singlyLinkedList.head);
-        ls.print(newHead);
+//        ls.print(singlyLinkedList.head);
+//        int length = ls.countElement(singlyLinkedList.head);
+//        System.out.println("total elements in list ::: " + length);
+//        ListNode newHead = ls.insertAtBeginning(singlyLinkedList.head, 20);
+//        ls.print(newHead);
+//        newHead = ls.insertAtBeginning(newHead, 30);
+//        ls.print(newHead);
+//        ls.insertAtEnd(singlyLinkedList.head, 60);
+//        ls.print(singlyLinkedList.head);
+//        ls.insertAtGivenPosition(singlyLinkedList.head, 2, 96);
+//        ls.print(singlyLinkedList.head);
+//        newHead = ls.deleteNodeFromBeginning(singlyLinkedList.head);
+//        ls.print(newHead);
+//
+//        ls.deleteLastNode(newHead);
+//        ls.print(newHead);
+//        ListNode head1 = ls.deleteAtGivenPosition(newHead, 1);
+//        ls.print(head1);
+//
+//        boolean isPresent = ls.searchKey(head1, 8);
+//        System.out.println("key  present in linked list ::: " + isPresent);
+//        ListNode reverseHead = ls.reverse(head1);
+//        ls.print(reverseHead);
 
-        ls.deleteLastNode(newHead);
-        ls.print(newHead);
-        ListNode head1 = ls.deleteAtGivenPosition(newHead, 1);
-        ls.print(head1);
+//        ListNode middle = ls.findMiddle(singlyLinkedList.head);
+//        System.out.println("MiddleNode ::: "+middle.data);
+//        ListNode node = ls.findNthNodeFromEnd2(singlyLinkedList.head, 3);
+//        System.out.println("nth Node from last  ::: "+node.data);
+//        ls.removeDuplicateFromSortedList(singlyLinkedList.head);
+//        ls.print(singlyLinkedList.head);
+//        ls.insertInSortedLinkedList(singlyLinkedList.head, 12);
+//        ls.print(singlyLinkedList.head);
+//        ls.removeGivenNode(singlyLinkedList.head, 12);
+//        ls.print(singlyLinkedList.head);
 
+        System.out.println("is Loop present ::: " + ls.checkIfLoop(singlyLinkedList.head));
+        System.out.println("starting Node of the loop :: " + ls.startingNodeOfLoop(singlyLinkedList.head).data);
+        ls.removeLoopIfPresent(singlyLinkedList.head);
+        ls.print(singlyLinkedList.head);
     }
 }
